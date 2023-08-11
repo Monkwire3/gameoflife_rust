361648f01b0a986e83d5ff0f4413912fc5bfed39
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2023-08-11 15:04:31 -0400

    implemented prettier display

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,23 @@ struct Grid {
 
 impl fmt::Display for Grid {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let grid_display = self.cells.iter().map(|row| row.iter().map(ToString::to_string).collect::<Vec<String>>().join(" ")).collect::<Vec<String>>().join("\n");
+        let grid_display = self.cells.iter()
+            .map(|row| row.iter()
+                .map(|&val| if val { "*" } else { " " })
+                .collect::<Vec<&str>>()
+                .join(" "))
+            .collect::<Vec<String>>()
+            .join("\n");
         write!(f, "{}", grid_display)
     }
-    
 }
 
 
 fn main() {
-    let grid = Grid { cells: [[false; GRID_SIZE]; GRID_SIZE]};
-    println!("{}", grid);
+    let mut grid = Grid { cells: [[true; GRID_SIZE]; GRID_SIZE]};
+    println!("Unmodified:\n{}", grid);
+    grid.cells[0][3] = false;
+    println!("Modified:\n{}", grid);
+
 
 }
